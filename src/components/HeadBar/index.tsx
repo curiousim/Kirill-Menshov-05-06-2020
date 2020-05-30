a2d2e83745e50ba827dffbed7aa95d9025c7cbeb
Kirill Menshov <kirill.menshov@gmail.com>
2020-05-30 12:30:04 +0300

    initial commit and structure

---
+++ src/components/HeadBar/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './HeadBar.css';
+
+export function HeadBar() {
+  return (
+    <nav className="hbar-container">
+      <div className="hbar-logo">Weather App</div>
+      <div className="hbar-controls">
+        <div className="hbar-temp">C &deg; / F &deg;</div>
+        <div className="hbar-nav">Home/Favorites</div>
+      </div>
+    </nav>
+  );
+}
