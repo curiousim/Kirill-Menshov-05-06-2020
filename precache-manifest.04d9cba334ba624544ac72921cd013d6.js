6f065b3d6318f3a2ffb9f33067840b4985835a05
Kirill Menshov <kirill.menshov@gmail.com>
2020-06-06 21:26:08 +0300

    Updates

---
+++ precache-manifest.04d9cba334ba624544ac72921cd013d6.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51171db898235c04fe4cc8e4ecc4e7e2",
+    "url": "/wearolo/index.html"
+  },
+  {
+    "revision": "e8ff43a91dba9db6afa1",
+    "url": "/wearolo/static/css/main.f6139b08.chunk.css"
+  },
+  {
+    "revision": "a2214a86fe107e32048c",
+    "url": "/wearolo/static/js/2.cd760754.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/wearolo/static/js/2.cd760754.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8ff43a91dba9db6afa1",
+    "url": "/wearolo/static/js/main.507b84ab.chunk.js"
+  },
+  {
+    "revision": "14b9137dc3efafd5af17",
+    "url": "/wearolo/static/js/runtime-main.59892231.js"
+  },
+  {
+    "revision": "7f93fbdb8a9d02fe414c0a0119b77bd4",
+    "url": "/wearolo/static/media/1.7f93fbdb.svg"
+  },
+  {
+    "revision": "6da13848d70f0a7673999356aeae825d",
+    "url": "/wearolo/static/media/11.6da13848.svg"
+  },
+  {
+    "revision": "c8cdb21695a664b4dbffeb1fc9631510",
+    "url": "/wearolo/static/media/12.c8cdb216.svg"
+  },
+  {
+    "revision": "35eb268dd7e8dfe694b55fec314a0b10",
+    "url": "/wearolo/static/media/13.35eb268d.svg"
+  },
+  {
+    "revision": "35eb268dd7e8dfe694b55fec314a0b10",
+    "url": "/wearolo/static/media/14.35eb268d.svg"
+  },
+  {
+    "revision": "7142c78bba5ddb0b77c3ccddcebe96ab",
+    "url": "/wearolo/static/media/15.7142c78b.svg"
+  },
+  {
+    "revision": "7142c78bba5ddb0b77c3ccddcebe96ab",
+    "url": "/wearolo/static/media/16.7142c78b.svg"
+  },
+  {
+    "revision": "7142c78bba5ddb0b77c3ccddcebe96ab",
+    "url": "/wearolo/static/media/17.7142c78b.svg"
+  },
+  {
+    "revision": "d235ab65ea35c3fcade47b8f5c6c4ae7",
+    "url": "/wearolo/static/media/18.d235ab65.svg"
+  },
+  {
+    "revision": "c915395f8c3e3571d734680505ceeb11",
+    "url": "/wearolo/static/media/19.c915395f.svg"
+  },
+  {
+    "revision": "80a5d865c63ba642a927daba54438305",
+    "url": "/wearolo/static/media/2.80a5d865.svg"
+  },
+  {
+    "revision": "c915395f8c3e3571d734680505ceeb11",
+    "url": "/wearolo/static/media/20.c915395f.svg"
+  },
+  {
+    "revision": "c915395f8c3e3571d734680505ceeb11",
+    "url": "/wearolo/static/media/21.c915395f.svg"
+  },
+  {
+    "revision": "67d63a14dbbef1d431f1851da197e0ae",
+    "url": "/wearolo/static/media/22.67d63a14.svg"
+  },
+  {
+    "revision": "b6eea2cf5e8a5ab5f57405addab863ae",
+    "url": "/wearolo/static/media/23.b6eea2cf.svg"
+  },
+  {
+    "revision": "c915395f8c3e3571d734680505ceeb11",
+    "url": "/wearolo/static/media/25.c915395f.svg"
+  },
+  {
+    "revision": "d235ab65ea35c3fcade47b8f5c6c4ae7",
+    "url": "/wearolo/static/media/26.d235ab65.svg"
+  },
+  {
+    "revision": "3def7c9fb95731ac622871fb6925d5f6",
+    "url": "/wearolo/static/media/29.3def7c9f.svg"
+  },
+  {
+    "revision": "b931d84d75a673fdd8ae1134fefd98ba",
+    "url": "/wearolo/static/media/3.b931d84d.svg"
+  },
+  {
+    "revision": "31d6bcb52be75457799196a5f209296b",
+    "url": "/wearolo/static/media/30.31d6bcb5.svg"
+  },
+  {
+    "revision": "d416343c72c4a60eedb99bf341a1e2ef",
+    "url": "/wearolo/static/media/31.d416343c.svg"
+  },
+  {
+    "revision": "507251e43591e9609e984ad9f04accd3",
+    "url": "/wearolo/static/media/32.507251e4.svg"
+  },
+  {
+    "revision": "a3f3f38d2c17971938ebd891fb6bbd0c",
+    "url": "/wearolo/static/media/33.a3f3f38d.svg"
+  },
+  {
+    "revision": "7e65840bf2249d8f7ba19fc029438545",
+    "url": "/wearolo/static/media/34.7e65840b.svg"
+  },
+  {
+    "revision": "ef1c6aa1ca72ea2157736318d24ede62",
+    "url": "/wearolo/static/media/35.ef1c6aa1.svg"
+  },
+  {
+    "revision": "ef1c6aa1ca72ea2157736318d24ede62",
+    "url": "/wearolo/static/media/36.ef1c6aa1.svg"
+  },
+  {
+    "revision": "bf91ac0f97cf729862dfaeb8fc25e037",
+    "url": "/wearolo/static/media/37.bf91ac0f.svg"
+  },
+  {
+    "revision": "bf91ac0f97cf729862dfaeb8fc25e037",
+    "url": "/wearolo/static/media/38.bf91ac0f.svg"
+  },
+  {
+    "revision": "c8cdb21695a664b4dbffeb1fc9631510",
+    "url": "/wearolo/static/media/39.c8cdb216.svg"
+  },
+  {
+    "revision": "b931d84d75a673fdd8ae1134fefd98ba",
+    "url": "/wearolo/static/media/4.b931d84d.svg"
+  },
+  {
+    "revision": "c8cdb21695a664b4dbffeb1fc9631510",
+    "url": "/wearolo/static/media/40.c8cdb216.svg"
+  },
+  {
+    "revision": "7142c78bba5ddb0b77c3ccddcebe96ab",
+    "url": "/wearolo/static/media/41.7142c78b.svg"
+  },
+  {
+    "revision": "7142c78bba5ddb0b77c3ccddcebe96ab",
+    "url": "/wearolo/static/media/42.7142c78b.svg"
+  },
+  {
+    "revision": "c915395f8c3e3571d734680505ceeb11",
+    "url": "/wearolo/static/media/43.c915395f.svg"
+  },
+  {
+    "revision": "3def7c9fb95731ac622871fb6925d5f6",
+    "url": "/wearolo/static/media/44.3def7c9f.svg"
+  },
+  {
+    "revision": "109368f632c5dafab6cbf47ec386f8d0",
+    "url": "/wearolo/static/media/5.109368f6.svg"
+  },
+  {
+    "revision": "109368f632c5dafab6cbf47ec386f8d0",
+    "url": "/wearolo/static/media/6.109368f6.svg"
+  },
+  {
+    "revision": "ad789581dd3bfe802dd2e6ea0c2f5caa",
+    "url": "/wearolo/static/media/7.ad789581.svg"
+  },
+  {
+    "revision": "ad789581dd3bfe802dd2e6ea0c2f5caa",
+    "url": "/wearolo/static/media/8.ad789581.svg"
+  }
+]);
